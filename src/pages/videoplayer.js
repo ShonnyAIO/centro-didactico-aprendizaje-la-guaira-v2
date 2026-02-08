43781a36b5b8211de05a477efdd19ea20a3c401b
Jonathan Torres <jonathan.torres.8096@gmail.com>
2026-02-08 02:43:42 -0400

    Cambios del video

--- src/pages/videoplayer.js
+++ src/pages/videoplayer.js
@@ -8,16 +8,19 @@ export default function VideoPlayer() {
         <>
             <div>
                 <PannellumVideo
-                    video={"https://firebasestorage.googleapis.com/v0/b/chat-153ee.appspot.com/o/RECORRIDO_LA_GUAIRA.mp4?alt=media&token=e0209f74-cc5c-45ce-bbfe-4f170047efba"}
+                    video={"https://firebasestorage.googleapis.com/v0/b/chat-153ee.appspot.com/o/RECORRIDO_LA_GUAIRA.mp4?alt=media&token=ceb427c1-901a-42b8-8966-9af30380fcda"}
                     loop
+                    autoplay
+                    controls={true}
+                    muted={false}
                     width="100%"
                     height="100vh"
-                    autoLoad
-                    autoPlay
-                    controls
                     pitch={10}
-                    yaw={180}
-                    hfov={200}
+                    yaw={20}
+                    hfov={120}
+                    minHfov={50}
+                    maxHfov={150}
+                    mouseZoom={true}
                 />
             </div>
         </>
